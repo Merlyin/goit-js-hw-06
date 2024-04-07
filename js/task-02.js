778f0cdbddbf4a5c5c0a845f918a55eeea2d1ac7
Merlyin <focastefan13@gmail.com>
2024-04-07 21:25:17 +0300

    task 2
    done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const documentElement = document.getElementById('ingredients');
+
+for(let i = 0; i < ingredients.length; i += 1) {
+	const listElem = document.createElement('li');
+	const textElem = document.createTextNode(ingredients[i]);
+    listElem.appendChild(textElem);
+    documentElement.appendChild(listElem);
+}
